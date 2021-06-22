e034aa1c713f08a2a86f99c5ee8cd46f9f39bb1c
Axel Gromat <axel.gromat@gmail.com>
2021-06-22 12:07:32 +0200

    Clean branch for exos

--- classes/Ovipare.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    class Ovipare extends Animal
-    {
-        public function identify()
-        {
-            return "Je suis un {$this->espece} qui s'appelle {$this->nom} et j'ai {$this->nbDePattes} pattes et je ponds des oeufs (accessoirement je suis proche aux dinosaures la plupart du temps)";
-        }
-    }
